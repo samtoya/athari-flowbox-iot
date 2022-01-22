a241af9d9b233fa60d6312eb39b048e04846dfc4
Joseph Akitoye <josephakitoye@gmail.com>
2022-01-22 00:19:02 +0000

    Add spring cloud kubernetes ribbon client
    Signed-off-by: Joseph Akitoye <josephakitoye@gmail.com>

--- src/main/java/com/grundfos/futurelab/athariflowbox/athariiotservice/AthariIotServiceApplication.java
+++ src/main/java/com/grundfos/futurelab/athariflowbox/athariiotservice/AthariIotServiceApplication.java
@@ -5,15 +5,17 @@ import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@RibbonClient(name = "athari-iot-service")
 @OpenAPIDefinition(info = @Info(description = "Athari.Flowbox IoT Service API", version = "1.0", title = "Athari.Flowbox IoT Service API"))
 public class AthariIotServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AthariIotServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AthariIotServiceApplication.class, args);
+    }
 
 }
