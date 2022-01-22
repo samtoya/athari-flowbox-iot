073759600356a78197363cffc1319f3a27d0a323
Joseph Akitoye <josephakitoye@gmail.com>
2022-01-22 20:49:12 +0000

    Add configurations for k8s, kustomize and skaffold
    - Code cleanups
    - Remove ribbon load balancer configuration
    - Add liquibase db migration engine
    - Configure multipe environments for deployment
    
    Signed-off-by: Joseph Akitoye <josephakitoye@gmail.com>

--- src/main/java/com/grundfos/futurelab/athariflowbox/athariiotservice/AthariIotServiceApplication.java
+++ src/main/java/com/grundfos/futurelab/athariflowbox/athariiotservice/AthariIotServiceApplication.java
@@ -5,12 +5,11 @@ import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-@RibbonClient(name = "athari-iot-service")
+@EnableDiscoveryClient
 @OpenAPIDefinition(info = @Info(description = "Athari.Flowbox IoT Service API", version = "1.0", title = "Athari.Flowbox IoT Service API"))
 public class AthariIotServiceApplication {
 
